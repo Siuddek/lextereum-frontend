316a51101374922ac50a62e1966fcb4ab2d97523
Siudek <maciej.siudek@bettersolutions.pl>
2020-08-12 14:12:01 +0200

    basic front layout

---
+++ src/app/components/file-chooser/file-chooser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-chooser',
+  templateUrl: './file-chooser.component.html',
+  styleUrls: ['./file-chooser.component.css']
+})
+export class FileChooserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

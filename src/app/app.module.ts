58898b2aada3520a427cb83530a3675f6c2479c3
Siudek <maciej.siudek@bettersolutions.pl>
2020-08-13 17:01:35 +0200

    load and display file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FileChooserComponent } from './components/file-chooser/file-chooser.component';
+import {FileChooserComponent, ImageOptionUploadSheetComponent} from './components/file-chooser/file-chooser.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MAT_BOTTOM_SHEET_DEFAULT_OPTIONS, MatBottomSheet} from '@angular/material/bottom-sheet';
+import {MatListModule} from '@angular/material/list';
+import {Overlay} from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileChooserComponent
+    FileChooserComponent,
+    ImageOptionUploadSheetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule
   ],
-  providers: [],
+  entryComponents: [
+    FileChooserComponent
+  ],
+  providers: [
+    {provide: MAT_BOTTOM_SHEET_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}},
+    MatBottomSheet,
+    Overlay
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/file-chooser/file-chooser.component.ts
+++ src/app/components/file-chooser/file-chooser.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-file-chooser',
   templateUrl: './file-chooser.component.html',
   styleUrls: ['./file-chooser.component.css']
 })
-export class FileChooserComponent implements OnInit {
+export class FileChooserComponent {
 
-  constructor() { }
+  documentImageUrl: string;
 
-  ngOnInit(): void {
+  constructor(private optionsSheet: MatBottomSheet) { }
+
+  openFileChooserSheet(): void {
+    const uploadOptionRef = this.optionsSheet.open(ImageOptionUploadSheetComponent, {disableClose: false});
+    uploadOptionRef.afterDismissed().subscribe((documentImage) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(documentImage);
+      reader.onload = (event) => {
+        this.documentImageUrl = event.target.result as string;
+      };
+    });
   }
 
+}
+
+@Component({
+  selector: 'app-image-option-upload-sheet',
+  templateUrl: './image-option-upload-sheet.html',
+  styleUrls: ['./image-option-upload-sheet.css']
+})
+export class ImageOptionUploadSheetComponent {
+  constructor(private uploadSheetMatBottomSheetRef: MatBottomSheetRef<ImageOptionUploadSheetComponent>) {
+  }
+
+  getImage(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      this.uploadSheetMatBottomSheetRef.dismiss(event.target.files[0]);
+    }
+  }
 }
